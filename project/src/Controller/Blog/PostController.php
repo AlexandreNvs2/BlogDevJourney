a697cf92101f381c0d95b096b804ffd233a9a3f3
AlexandreNvs2 <alexandre.neves0402@gmail.com>
2024-04-06 00:43:57 +0200

    Création de templates d'erreur + création de show qui utilise paramConverter

--- project/src/Controller/Blog/PostController.php
+++ project/src/Controller/Blog/PostController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller\Blog;
 
+use App\Entity\Post\Post;
 use App\Repository\Post\PostRepository;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class PostController extends AbstractController
 {
@@ -21,5 +23,21 @@ class PostController extends AbstractController
             #permet d'utiliser la variables $posts au sein du twig
             'posts' => $posts
             ]);
+    }
+    /**
+     * Cette route capte un 'slug' de l'URL et le passe à la méthode show().
+     * @param Post $post
+     * @param PostRepository $postRepository
+     * @return Response
+     */
+    // Cette route capte un 'slug' de l'URL et le passe à la méthode show().
+    #[Route('/article/{slug}', name: 'post.show', methods: ['GET'])]
+    public function show(Post $post): Response
+    {
+        // ParamConverter automatiquement convertit 'slug' en entité Post.
+        // Si aucun Post trouvé pour 'slug', une erreur 404 est générée.
+
+        // Génère le HTML en utilisant 'post' trouvé pour le template spécifié.
+        return $this->render('pages/blog/show.html.twig', ['post' => $post]);
     }
 }
