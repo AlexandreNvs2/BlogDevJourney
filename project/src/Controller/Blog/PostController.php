8f163544097ef3db038dee82eef0fffaf51fefca
AlexandreNvs2 <alexandre.neves0402@gmail.com>
2024-04-09 11:26:45 +0200

    Mise en place d'une barre dd recherche, logique dans PostRepository.php

--- project/src/Controller/Blog/PostController.php
+++ project/src/Controller/Blog/PostController.php
@@ -3,26 +3,43 @@
 namespace App\Controller\Blog;
 
 use App\Entity\Post\Post;
+use App\Model\SearchData;
 use App\Repository\Post\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\SearchType;
+
 
 
 class PostController extends AbstractController
 {
-    #[Route('/', name: 'post.index' , methods: ['GET'])]
-    public function index(PostRepository $postRepository ,Request $request): Response
-    {
-        $posts = $postRepository->findPublished($request->query->getInt('page', 1));
+    #[Route('/', name: 'post.index', methods: ['GET'])]
+    public function index(
+        PostRepository $postRepository,
+        Request $request
+    ): Response {
+        $searchData = new SearchData();
+        $form = $this->createForm(SearchType::class, $searchData);
 
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $searchData->page = $request->query->getInt('page', 1);
+            $posts = $postRepository->findBySearch($searchData);
 
-        return $this->render('pages/post/index.html.twig',[
-            #permet d'utiliser la variables $posts au sein du twig
-            'posts' => $posts
+            return $this->render('pages/post/index.html.twig', [
+                'form' => $form->createView(),
+                'posts' => $posts
             ]);
+        }
+
+        return $this->render('pages/post/index.html.twig', [
+            'form' => $form->createView(),
+            'posts' => $postRepository->findPublished($request->query->getInt('page', 1))
+        ]);
     }
+
     /**
      * Cette route capte un 'slug' de l'URL et le passe à la méthode show().
      * @param Post $post
--- project/src/Repository/Post/PostRepository.php
+++ project/src/Repository/Post/PostRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository\Post;
 use App\Entity\Post\Category;
 use App\Entity\Post\Post;
 use App\Entity\Post\Tag;
+use App\Model\SearchData;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Knp\Component\Pager\Pagination\PaginationInterface;
@@ -60,4 +61,41 @@ class PostRepository extends ServiceEntityRepository
 
         return $posts;
     }
+    /**
+     * Get published posts thanks to Search Data value
+     *
+     * @param SearchData $searchData
+     * @return PaginationInterface
+     */
+    public function findBySearch(SearchData $searchData): PaginationInterface
+    {
+        $data = $this->createQueryBuilder('p')
+            ->where('p.state LIKE :state')
+            ->setParameter('state', '%STATE_PUBLISHED%')
+            ->addOrderBy('p.createdAt', 'DESC');
+
+        if (!empty($searchData->q)) {
+            $data = $data
+                ->join('p.tags', 't')
+                ->andWhere('p.title LIKE :q')
+                ->orWhere('t.name LIKE :q')
+                ->setParameter('q', "%{$searchData->q}%");
+        }
+
+        if (!empty($searchData->categories)) {
+            $data = $data
+                ->join('p.categories', 'c')
+                ->andWhere('c.id IN (:categories)')
+                ->setParameter('categories', $searchData->categories);
+        }
+
+        $data = $data
+            ->getQuery()
+            ->getResult();
+
+        $posts = $this->paginator->paginate($data, $searchData->page, 9);
+
+        return $posts;
+    }
+
 }
