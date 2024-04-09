cc1fee01ce49ff35bc2a337ba0e55df4b2d52167
AlexandreNvs2 <alexandre.neves0402@gmail.com>
2024-04-09 12:05:01 +0200

    TestBlog SearchBarWork and return No Item

--- project/tests/Functionnal/Post/BlogTest.php
+++ project/tests/Functionnal/Post/BlogTest.php
@@ -3,9 +3,14 @@
 namespace App\Tests\Functionnal\Post;
 
 
+use App\Entity\Post\Tag;
+use App\Entity\Post\Post;
+use App\Entity\Post\Category;
+use App\Repository\Post\CategoryRepository;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
 
 class BlogTest extends WebTestCase
 {
@@ -51,5 +56,119 @@ class BlogTest extends WebTestCase
         $posts = $crawler->filter('div.card');
         $this->assertGreaterThanOrEqual(1, count($posts));
     }
+
+    public function testFilterSystemWorks(): void
+    {
+        // Création d'un client pour simuler un navigateur
+        $client = static::createClient();
+
+        // Récupération du service de génération d'URL
+        $urlGeneratorInterface = $client->getContainer()->get('router');
+
+        // Récupération du gestionnaire d'entités
+        $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
+
+        // Récupération du repository des Posts et Categories
+        $postRepository = $entityManager->getRepository(Post::class);
+        $categoryRepository = $entityManager->getRepository(Category::class);
+
+        // Trouver un Post et récupérer son premier Tag
+        $post = $postRepository->findOneBy([]);
+        $tag = $post->getTags()[0];
+
+        // Trouver une Category
+        $category = $categoryRepository->findOneBy([]);
+
+        // Faire une requête GET à la page d'index des Posts
+        $crawler = $client->request(Request::METHOD_GET, $urlGeneratorInterface->generate('post.index'));
+
+        // Vérification de la réponse HTTP
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+
+        // Définir les termes de recherche à partir du titre du Post et du nom du Tag
+        $searchs = [
+            substr($post->getTitle(), 0, 3),
+            substr($tag->getName(), 0, 3)
+        ];
+
+        // Itérer sur les termes de recherche
+        foreach ($searchs as $search) {
+            // Soumettre le formulaire de recherche avec le terme de recherche et une catégorie sélectionnée
+            $form = $crawler->filter('form[name=search]')->form([
+                'search[q]' => $search,
+                'search[categories][0]' => 1
+            ]);
+
+            $crawler = $client->submit($form);
+
+            // Vérifier à nouveau la réponse HTTP
+            $this->assertResponseIsSuccessful();
+            $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+            $this->assertRouteSame('post.index');
+
+            // Compter le nombre de Posts retournés
+            $nbPosts = count($crawler->filter('div.card'));
+            $posts = $crawler->filter('div.card');
+            $count = 0;
+
+            // Itérer sur chaque Post et vérifier si le terme de recherche est présent dans le titre ou le Tag
+            foreach ($posts as $index => $post) {
+                $title = $crawler->filter("div.card h5")->getNode($index);
+                if (
+                    str_contains($title->textContent, $search) ||
+                    str_contains($tag->getName(), $search)
+                ) {
+                    $postCategories = $crawler->filter('div.card div.badges')->getNode($index)->childNodes;
+
+                    for ($i = 1; $i < $postCategories->count(); $i++) {
+                        $postCategory = $postCategories->item($i);
+                        $name = trim($postCategory->textContent);
+
+                        if ($name === $category->getName()) {
+                            $count++;
+                        }
+                    }
+                }
+            }
+
+            // Vérifier si le nombre de Posts avec la catégorie correcte correspond au nombre total de Posts retournés
+            $this->assertEquals($nbPosts, $count);
+        }
+    }
+
+
+    public function testFilterSystemReturnsNoItems(): void
+    {
+        // Création d'un client pour simuler un navigateur
+        $client = static::createClient();
+
+        // Récupération du service de génération d'URL
+        $urlGeneratorInterface = $client->getContainer()->get('router');
+
+        // Faire une requête GET à la page d'index des Posts
+        $crawler = $client->request(Request::METHOD_GET, $urlGeneratorInterface->generate('post.index'));
+
+        // Vérification de la réponse HTTP
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+
+        // Soumettre le formulaire de recherche avec un terme de recherche qui ne correspond à aucun Post
+        $form = $crawler->filter('form[name=search]')->form([
+            'search[q]' => 'aazzeerrttyy'
+        ]);
+
+        $crawler = $client->submit($form);
+
+        // Vérifier à nouveau la réponse HTTP
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        $this->assertRouteSame('post.index');
+
+        // Vérifier que le formulaire de recherche est présent mais aucun Post n'est retourné
+        $this->assertSelectorExists('form[name=search]');
+        $this->assertSelectorNotExists('div.card');
+    }
+
 
 }
