b10de2ac041dd934bd2289ecc8f33b54c05f7570
AlexandreNvs2 <alexandre.neves0402@gmail.com>
2024-04-11 01:33:21 +0200

    CommentType.php + Gestion du commentaire

---
+++ project/src/Controller/Blog/CommentController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller\Blog;
+
+use App\Entity\Post\Comment;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CommentController extends AbstractController
+{
+
+}
--- project/src/Controller/Blog/PostController.php
+++ project/src/Controller/Blog/PostController.php
@@ -5,12 +5,14 @@ namespace App\Controller\Blog;
 use App\Entity\Post\Post;
 use App\Model\SearchData;
 use App\Repository\Post\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\SearchType;
-
+use App\Form\CommentType;
+use App\Entity\Post\Comment;
 
 
 class PostController extends AbstractController
@@ -46,13 +48,35 @@ class PostController extends AbstractController
      * @return Response
      */
     // Cette route capte un 'slug' de l'URL et le passe à la méthode show().
-    #[Route('/article/{slug}', name: 'post.show', methods: ['GET'])]
-    public function show(Post $post): Response
+    #[Route('/article/{slug}', name: 'post.show', methods: ['GET','POST'])]
+    public function show(Post $post, EntityManagerInterface $manager, Request $request): Response
     {
+
+        $form = $this->createForm(CommentType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $comment = new Comment;
+            $comment->setPost($post);
+            $comment->setAuthor($this->getUser());
+
+            $comment->setContent($form->getData()['content']);
+            $manager->persist($comment);
+            $manager->flush();
+
+            $this->addFlash("success", "Votre commentaire à été poster avec succès !");
+
+            return $this->redirectToRoute('post.show', ['slug' => $post->getSlug()]);
+        }
         // ParamConverter automatiquement convertit 'slug' en entité Post.
         // Si aucun Post trouvé pour 'slug', une erreur 404 est générée.
 
         // Génère le HTML en utilisant 'post' trouvé pour le template spécifié.
-        return $this->render('pages/post/show.html.twig', ['post' => $post]);
+        return $this->render('pages/post/show.html.twig', [
+            'post' => $post,
+            'form' => $form->createView()
+
+        ],
+        );
     }
 }
